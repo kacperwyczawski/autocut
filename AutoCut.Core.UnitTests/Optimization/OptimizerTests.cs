5a39de5bf45a30886f9f9d2252008771f7aef010
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-19 18:41:39 +0100

    Add simple test for optimizer

---
+++ AutoCut.Core.UnitTests/Optimization/OptimizerTests.cs
@@ -0,0 +1,28 @@
+﻿using AutoCut.Core.Models.Panels;
+using AutoCut.Core.Optimization;
+
+namespace AutoCut.Core.UnitTests.Optimization;
+
+public class OptimizerTests
+{
+    [Fact]
+    public void SinglePanel()
+    {
+        // arrange
+        var panels = new List<Panel> { new(100, 100) };
+        var stockPanel = new StockPanel(2800, 2070, 18);
+        var optimizer = new Optimizer();
+
+        var expected = new OptimizationResult(
+            1,
+            new OptimizerSettings(),
+            new List<StockPanel> { stockPanel },
+            new List<OptimizedPanel> { new(100, 100, 0, 0) });
+        
+        // act
+        var actual = optimizer.Optimize(stockPanel, panels);
+        
+        // assert
+        Assert.Equal(expected, actual);
+    }
+}

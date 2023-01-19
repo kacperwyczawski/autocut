1d3b5ef5f60b323d9d13f9ec36a56b7e7d4704b0
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-19 18:13:31 +0100

    Add tests for decompression

---
+++ AutoCut.Core.UnitTests/Models/Panels/PanelUtilitiesTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoCut.Core.Models.Panels;
+
+namespace AutoCut.Core.UnitTests.Models.Panels;
+
+public class PanelUtilitiesTests
+{
+    [Fact]
+    public void Decompression()
+    {
+        // arrange
+        var panelToDecompress = new CompressedPanel(4, 20, 5);
+        var expectedPanels = new List<Panel>
+        {
+            new (4, 20),
+            new (4, 20),
+            new (4, 20),
+            new (4, 20),
+            new (4, 20)
+        };
+        
+        // act
+        var decompressedPanels = panelToDecompress.Decompress();
+        
+        // assert
+        Assert.Equal(expectedPanels, decompressedPanels);
+    }
+}

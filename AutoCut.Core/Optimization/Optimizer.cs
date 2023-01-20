3030937a738cb9aaf5286b0ab5a93e7f40630c21
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-20 17:00:26 +0100

    Reverse comparer

--- AutoCut.Core/Optimization/Optimizer.cs
+++ AutoCut.Core/Optimization/Optimizer.cs
@@ -5,6 +5,11 @@ namespace AutoCut.Core.Optimization;
 public class Optimizer
 {
     public OptimizerSettings Settings { get; set; }
+    
+    /// <summary>
+    /// Reversed comparer (compares b to a). See: https://stackoverflow.com/a/24306467/18682712
+    /// </summary>
+    private readonly Comparer<Panel> _reversedComparer = Comparer<Panel>.Create((a, b) => b.CompareTo(a));
 
     public Optimizer(OptimizerSettings settings)
     {
@@ -25,7 +30,7 @@ public class Optimizer
     {
         // TODO: check if list or enumerable is faster in freeRectangles
         var usedStockPanels = new List<StockPanel>();
-        var freeRectangles = new SortedSet<PositionedPanel>();
+        var freeRectangles = new SortedSet<PositionedPanel>(_reversedComparer);
         var panelsToProcess = panels.Order().ToList();
         var optimizedPanels = new List<PositionedPanel>();
 

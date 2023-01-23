2fa6f1140226b365dfe3f5c24c79fe439c8eb37b
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-23 13:47:26 +0100

    Add MudBlazor

--- AutoCut.Frontend/Program.cs
+++ AutoCut.Frontend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AutoCut.Frontend;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();

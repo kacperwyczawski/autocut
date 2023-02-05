0dd0f8e2dbaffca9df6806ce294c39c8dff42658
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-02-05 11:30:11 +0100

    feat: and panel state management using fluxor

--- AutoCut.Frontend/Program.cs
+++ AutoCut.Frontend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AutoCut.Frontend;
+using Fluxor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddFluxor(options => options.ScanAssemblies(typeof(Program).Assembly));
 
 await builder.Build().RunAsync();

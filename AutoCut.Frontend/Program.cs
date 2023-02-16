346ca6bd9e5ae6f8124346fdf768d2f1e66f15c1
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-02-16 20:48:47 +0100

    Feat: add support for redux dev tools

--- AutoCut.Frontend/Program.cs
+++ AutoCut.Frontend/Program.cs
@@ -11,6 +11,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-builder.Services.AddFluxor(options => options.ScanAssemblies(typeof(Program).Assembly));
+builder.Services.AddFluxor(options =>
+{
+    options.ScanAssemblies(typeof(Program).Assembly);
+#if DEBUG
+    options.UseReduxDevTools();
+#endif
+});
 
 await builder.Build().RunAsync();

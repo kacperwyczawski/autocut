3cf10130a6e9368d5b7988b7035629cacb97265f
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-03-11 15:07:57 +0100

    feat(frontend): remove MudBlazor, update action buttons, update app.css, add normalize.css

--- AutoCut.Frontend/Program.cs
+++ AutoCut.Frontend/Program.cs
@@ -2,7 +2,6 @@ using AutoCut.Frontend;
 using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,7 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
 builder.Services.AddFluxor(options =>
 {
     options.ScanAssemblies(typeof(Program).Assembly);

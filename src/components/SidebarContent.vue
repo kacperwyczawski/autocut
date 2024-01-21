b89b256339b4d0e0cf46e94b0a4dbf27868059e5
Kacper Wyczawski <kacper@wyczawski.dev>
2024-01-21 15:15:24 +0100

    feat: sidebar and main buttons

---
+++ src/components/SidebarContent.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="p-2 w-80 min-h-full bg-base-200">
+        <div role="tablist" class="tabs tabs-boxed p-0">
+            <a role="tab" class="tab">Panels</a>
+            <a role="tab" class="tab tab-active">Cuts</a>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,39 @@
 <script setup lang="ts">
+import SidebarContent from '../components/SidebarContent.vue'
+import { Info, Menu } from 'lucide-vue-next';
 </script>
 
 <template>
-  <p class="">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt officia numquam quidem dolore beatae excepturi odio nesciunt doloremque, sit magnam sint quasi, dolorem recusandae deleniti odit aut, quisquam autem architecto.
-  </p>
+  <div class="drawer md:drawer-open">
+    <input id="sidebar" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content p-2">
+      <div class="flex gap-2">
+        <label for="sidebar" class="btn drawer-button md:hidden">
+          <Menu />
+        </label>
+        <button class="btn">
+          Reset panels
+        </button>
+        <button class="btn btn-primary">
+          Optimize
+        </button>
+      </div>
+      <div role="alert" class="alert alert-info mt-2">
+        <Info />
+        <span>
+          Add some panels first
+        </span>
+      </div>
+      <div role="alert" class="alert alert-info mt-2 md:hidden">
+        <Info />
+        <span>
+          You can open the sidebar by clicking the menu icon
+        </span>
+      </div>
+    </div>
+    <div class="drawer-side">
+      <label for="sidebar" aria-label="close sidebar" class="drawer-overlay"></label>
+      <SidebarContent />
+    </div>
+  </div>
 </template>

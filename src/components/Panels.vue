aff43dc2077afc8b5e6a6044c17c6c8bf5997ec6
Kacper Wyczawski <kacper@wyczawski.dev>
2024-01-27 12:04:26 +0100

    feat: delete panels

--- src/components/Panels.vue
+++ src/components/Panels.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-
 const panels = defineModel<Panel[]>()
-
-// function deletePanel(index: number) {
-//     panels.value.splice(index, 1);
-// }
 </script>
 <template>
     <table class="table">
@@ -22,7 +17,7 @@ const panels = defineModel<Panel[]>()
                 <td>{{ panel.width }}</td>
                 <td>{{ panel.quantity }}</td>
                 <td>
-                    <button class="btn btn-square btn-error btn-xs">
+                    <button class="btn btn-square btn-error btn-xs" @click="panels?.splice(index, 1)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                             class="lucide lucide-x">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,18 +6,7 @@ import { ref } from 'vue';
 const currentTab = ref('Panels');
 const tabList = ['Panels', 'Cuts'];
 
-const panels = ref<Panel[]>([
-    { length: 1000, width: 500, quantity: 200 },
-    { length: 800, width: 400, quantity: 300 },
-    { length: 1200, width: 600, quantity: 100 },
-    { length: 600, width: 300, quantity: 400 },
-    { length: 900, width: 700, quantity: 200 },
-    { length: 1100, width: 900, quantity: 500 },
-    { length: 700, width: 200, quantity: 300 },
-    { length: 1400, width: 800, quantity: 200 },
-    { length: 1500, width: 1000, quantity: 300 },
-    { length: 1300, width: 600, quantity: 400 },
-]);
+const panels = ref<Panel[]>([]);
 </script>
 
 <template>
@@ -25,7 +14,7 @@ const panels = ref<Panel[]>([
     <input id="sidebar" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content p-2">
       <ControlPanel @add-panel="(panel) => panels.push(panel)" />
-      <div role="alert" class="alert alert-info mt-2">
+      <div v-if="panels.length === 0" role="alert" class="alert alert-info mt-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
           stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
           class="lucide lucide-info">
@@ -54,17 +43,18 @@ const panels = ref<Panel[]>([
       <label for="sidebar" aria-label="close sidebar" class="drawer-overlay"></label>
       <div class="p-2 w-80 min-h-full bg-base-200">
         <div role="tablist" class="tabs tabs-boxed p-0">
-            <a v-for="tab in tabList" :key="tab" role="tab" class="tab" :class="{ 'bg-secondary text-secondary-content': currentTab === tab }" @click="currentTab = tab">
-                {{ tab }}
-            </a>
+          <a v-for="tab in tabList" :key="tab" role="tab" class="tab"
+            :class="{ 'bg-secondary text-secondary-content': currentTab === tab }" @click="currentTab = tab">
+            {{ tab }}
+          </a>
         </div>
         <div v-if="currentTab === 'Panels'">
-            <Panels v-model="panels"/>
-            <button class="btn btn-error">
-                Delete all panels
-            </button>
+          <Panels v-model="panels" />
+          <button v-show="panels.length !== 0" class="btn btn-error btn-outline" @click="panels = []">
+            Delete all panels
+          </button>
         </div>
-    </div>
+      </div>
     </div>
   </div>
 </template>

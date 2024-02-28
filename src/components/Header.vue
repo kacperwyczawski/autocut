d2bb47383ad8062b51404cc73159eb5ebcc93f19
Kacper Wyczawski <kacper@wyczawski.dev>
2024-02-28 18:32:23 +0100

    feat: optimize navbar for mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,77 +1,41 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
-
-// please keep in sync with themes in tailwind.config.js (+ lime)
-const themes = [
-  "light",
-  "dark",
-  "cupcake",
-  "bumblebee",
-  "synthwave",
-  "retro",
-  "cyberpunk",
-  "dracula",
-  "night",
-  "coffee",
-  "dim",
-  "nord",
-  "lime",
-];
-
-const currentTheme = ref(localStorage.getItem("theme") || "coffee");
-watch(currentTheme, (x) => {
-  localStorage.setItem("theme", x);
-});
+import HeaderButtons from "@/components/HeaderButtons.vue";
 </script>
 <template>
-  <header class="flex gap-2 p-2 border-b border-base-300">
-    <h1 class="grow flex items-center">
-      <RouterLink to="/" class="text-3xl font-bold tracking-tight">
-        AutoCut
-      </RouterLink>
-    </h1>
-    <a
-      class="btn"
-      href="https://github.com/kacperwyczawski/autocut/commits/main/"
-    >
-      Changelog
-    </a>
-    <a class="btn" href="https://github.com/kacperwyczawski/autocut">
-      Source
-    </a>
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn">
-        Theme
-        <svg
-          width="12px"
-          height="12px"
-          class="h-2 w-2 fill-current opacity-60 inline-block"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 2048 2048"
-        >
-          <path
-            d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"
-          ></path>
-        </svg>
-      </div>
-      <ul
-        tabindex="0"
-        class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
-      >
-        <li v-for="theme in themes">
-          <input
-            v-model="currentTheme"
-            type="radio"
-            name="theme-dropdown"
-            class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-            :aria-label="theme"
-            :value="theme"
-          />
-        </li>
-      </ul>
-    </div>
-    <RouterLink to="/settings">
-      <button class="btn">Settings</button>
+  <header class="navbar border-b border-base-300">
+    <RouterLink to="/" class="navbar-start text-3xl font-bold tracking-tight">
+      AutoCut
     </RouterLink>
+    <div class="navbar-end hidden md:flex gap-2">
+      <HeaderButtons :extended-themes="true" />
+    </div>
+    <div class="navbar-end box md:hidden">
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-menu"
+          >
+            <line x1="4" x2="20" y1="12" y2="12" />
+            <line x1="4" x2="20" y1="6" y2="6" />
+            <line x1="4" x2="20" y1="18" y2="18" />
+          </svg>
+        </div>
+        <ul
+          tabindex="0"
+          class="dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 flex flex-col gap-2 items-stretch"
+        >
+          <HeaderButtons :extended-themes="false" />
+        </ul>
+      </div>
+    </div>
   </header>
 </template>
---
+++ src/components/HeaderButtons.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+
+defineProps<{
+  extendedThemes: boolean;
+}>();
+
+const themes = [
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "dracula",
+  "night",
+  "coffee",
+  "dim",
+  "nord",
+  "lime", // additional custom theme
+];
+
+const currentTheme = ref(localStorage.getItem("theme") || "retro");
+watch(currentTheme, (x) => {
+  localStorage.setItem("theme", x);
+});
+</script>
+<template>
+  <div v-if="extendedThemes" class="dropdown">
+    <div tabindex="0" role="button" class="btn">
+      Theme
+      <svg
+        width="12px"
+        height="12px"
+        class="h-2 w-2 fill-current opacity-60 inline-block"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 2048 2048"
+      >
+        <path
+          d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"
+        ></path>
+      </svg>
+    </div>
+    <ul
+      tabindex="0"
+      class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
+    >
+      <li v-for="theme in themes">
+        <input
+          v-model="currentTheme"
+          type="radio"
+          name="theme-dropdown"
+          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+          :aria-label="theme"
+          :value="theme"
+        />
+      </li>
+    </ul>
+  </div>
+  <div v-else class="form-control">
+    <label class="label cursor-pointer">
+      <span class="label-text">Dark mode: </span>
+      <input type="checkbox" value="coffee" class="checkbox theme-controller" />
+    </label>
+  </div>
+  <a
+    class="btn"
+    href="https://github.com/kacperwyczawski/autocut/commits/main/"
+  >
+    Changelog
+  </a>
+  <a class="btn" href="https://github.com/kacperwyczawski/autocut">Source</a>
+  <RouterLink to="/settings" class="btn">Settings</RouterLink>
+</template>

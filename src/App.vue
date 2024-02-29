89ab43de7107eafdef34d2d9f463de8dccdf29f3
Kacper Wyczawski <kacper@wyczawski.dev>
2024-02-29 19:06:56 +0100

    feat: keep alive home view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 import Header from "./components/Header.vue";
 </script>
 
 <template>
   <div class="grid grid-rows-[4rem_calc(100dvh_-_4rem)]">
     <Header />
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <KeepAlive include="HomeView">
+        <component :is="Component" />
+      </KeepAlive>
+    </RouterView>
   </div>
 </template>

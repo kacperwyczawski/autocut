da518a48b977f9098d43adaf3fe176ce0e443a08
Kacper Wyczawski <kacper@wyczawski.dev>
2024-04-17 14:24:34 +0200

    fix: remove console.debug

--- src/core/optimize.ts
+++ src/core/optimize.ts
@@ -143,7 +143,6 @@ export function optimize(
 			};
 			optimizedSheets.push(newOptimizedSheet);
 		}
-		console.debug(structuredClone(optimizedSheets));
 	}
 	const endTime = performance.now();
 	return {
